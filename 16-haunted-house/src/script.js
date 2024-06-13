2e2fee4ff1060eeeb9dd7f3e32396369eb5bffd7
minakim <minakim.dev@gmail.com>
2024-06-13 11:35:30 +0200

    16 -ing

---
+++ 16-haunted-house/src/script.js
@@ -0,0 +1,211 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Timer } from 'three/addons/misc/Timer.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * House
+ */
+const houseMeasurments = {
+    width:  4,
+    height: 2.5,
+    depth:  4
+}
+
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshStandardMaterial()
+)
+
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+// house group
+const house = new THREE.Group()
+scene.add(house)
+
+// walls
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(houseMeasurments.width, houseMeasurments.height, houseMeasurments.depth),
+    new THREE.MeshStandardMaterial()
+)
+walls.position.y += houseMeasurments.height * 0.5
+house.add(walls)
+
+// roof
+const roofMeasurment = {
+    height:         1.5,
+    radialSegments: 4,
+    radius:         3.5
+
+}
+
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(roofMeasurment.radius, roofMeasurment.height, roofMeasurment.radialSegments),
+    new THREE.MeshStandardMaterial()
+)
+roof.position.y += houseMeasurments.height + roofMeasurment.height * 0.5
+roof.rotation.y = Math.PI * 0.25
+house.add(roof)
+
+// door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2, 2.2),
+    new THREE.MeshStandardMaterial()
+)
+
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
+// Bushes
+const bushGeometyry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial()
+
+const bush1 = new THREE.Mesh(bushGeometyry, bushMaterial)
+bush1.scale.setScalar(0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+
+const bush2 = new THREE.Mesh(bushGeometyry, bushMaterial)
+bush2.scale.setScalar(0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+
+const bush3 = new THREE.Mesh(bushGeometyry, bushMaterial)
+bush3.scale.setScalar(0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometyry, bushMaterial)
+bush4.scale.setScalar(0.15)
+bush4.position.set(-1, 0.05, 2.6)
+
+house.add(bush1, bush2, bush3, bush4)
+
+// Graves
+const graveMeasurments = 
+{
+    width:  0.6,
+    height: 0.8,
+    depth:  0.2
+}
+
+const graveGeometry = new THREE.BoxGeometry(graveMeasurments.width, graveMeasurments.height, graveMeasurments.depth)
+const graveMaterial = new THREE.MeshStandardMaterial()
+
+const graves = new THREE.Group()
+scene.add(graves)
+
+for (let i = 0; i < 30; i++)
+{
+  const angle = Math.random() * Math.PI * 2
+    const radius = houseMeasurments.width - 1 + Math.random() * 4
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius
+
+    // Mesh
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.position.x = x
+    grave.position.y = Math.random() * graveMeasurments.height * 0.5
+    grave.position.z = z
+
+    grave.rotation.x = (Math.random() - 0.5) * 0.4
+    grave.rotation.y = (Math.random() - 0.5) * 0.4
+    grave.rotation.z = (Math.random() - 0.5) * 0.4
+
+    // add to graves group
+    graves.add(grave)
+}
+
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+scene.add(ambientLight)
+
+// Directional light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+directionalLight.position.set(3, 2, -8)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 5
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const timer = new Timer()
+
+const tick = () =>
+{
+    // Timer
+    timer.update()
+    const elapsedTime = timer.getElapsed()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
